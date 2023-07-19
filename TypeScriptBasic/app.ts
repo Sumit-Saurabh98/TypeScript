c2712da17adbc5739dcf073f430ca28c6b316da0
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-07-19 09:59:18 +0530

    union types any tuple

--- TypeScriptBasic/app.ts
+++ TypeScriptBasic/app.ts
@@ -1,58 +1,20 @@
-
-function add(num1: number, num2: number){
-    return num1 + num2;
-}
-
-const num1  = 5;
-const num2 = 10;
-
-const result = add(num1, num2);
-console.log(result);
-
-console.log(typeof (result), "checking types");
-
-let number1 : number;
-
-number1 = 5;
-
-console.log(number1);
-
-// const product:{
-//     id:string;
-//     price:number;
-//     tags:string[];
-//     details:{
-//         title:string;
-//         description:string;
-//     }
-// } 
-const person= {
-    id: 'abc1',
-    price: 12.99,
-    tags: ['great-offer', 'hot-and-new'],
-    details: {
-      title: 'Red Carpet',
-      description: 'A great carpet - almost brand-new!'
+function add(input1: number | string, input2: number | string) {
+    let result;
+  
+    if (typeof input1 === "number" && typeof input2 === "number") {
+      result = input1 + input2;
+    } else {
+      result = input1.toString() + input2.toString();
     }
+    return result;
   }
-
-  console.log(person);
-
-  for(const tag of person.tags){
-    console.log(tag.toLocaleUpperCase());
-  }
-
-  let arrayTypes: any[];
-  arrayTypes = ["sunny", 4]
-
-
-
-// Data Types in TypeScript
-/*
-number
-string
-boolean
-object
-array
-any
-*/
+  
+  const combinedNumber = add(5, 6);
+  console.log(combinedNumber);
+  
+  const combinedNames = add("sumit", "saurabh");
+  console.log(combinedNames);
+  
+  /*
+  union types accepts more than 1 types and we can pipe them. number | string | boolean
+  */
---
+++ TypeScriptBasic/core-types.ts
@@ -0,0 +1,73 @@
+
+function add(num1: number, num2: number){
+    return num1 + num2;
+}
+
+const num1  = 5;
+const num2 = 10;
+
+const result = add(num1, num2);
+console.log(result);
+
+console.log(typeof (result), "checking types");
+
+let number1 : number;
+
+number1 = 5;
+
+console.log(number1);
+
+const person:{
+    id:string;
+    price:number;
+    tags:string[];
+    details:{
+        title:string;
+        description:string;
+    };
+    author:[number, string]
+} 
+= {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+      title: 'Red Carpet',
+      description: 'A great carpet - almost brand-new!'
+    },
+    author:[2, 'john'] // tuple consist only two elements,  the first is the number and the second is the string
+  }
+
+  // console.log(person);
+
+  for(const tag of person.tags){
+    console.log(tag.toLocaleUpperCase());
+  }
+
+
+  person.author.push("number")  // exception in tuple that it accepts push method
+
+
+  console.log(typeof person.author, "author type")
+
+  let arrayTypes: any[];
+  arrayTypes = ["sunny", 4]
+
+  console.log(person);
+
+
+  enum target {"ANIMAL", "HUMAN", "BIRDS"};
+
+  console.log(target.ANIMAL); // it gives 0 because it starts from 0, we can set any value to it. like {ANIMAL=100}
+
+// Data Types in TypeScript
+/*
+number
+string
+boolean
+object
+array
+any
+tuple
+enum
+*/
